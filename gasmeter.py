7919b46c36d7d4fcd78a84bf46efda7c357520bf
jddayley <jddayley@gmail.com>
2024-01-21 11:42:30 -0500

    Update gasmeter.py

--- gasmeter.py
+++ gasmeter.py
@@ -1,301 +1,164 @@
-#!/usr/bin/env python3
-"""Read gas meter and publish as MQTT"""
-import sys
+import torchvision
+import torch
+import torchvision.transforms as transforms
+import PIL.Image as Image
+import cv2 
 import os
-import json
-from datetime import datetime
-from datetime import timedelta
-import time
-import statistics
-import collections
-import paho.mqtt.client as mqttClient
-import cv2
-import gas_meter_reader
-import mariadb
 import sys
-from datetime import datetime
-from image_predict import *
+from threshtransform import *
 from globals import *
-CONNECTED = False # MQTT connected
+import torchvision.models as models
+import torch.nn as nn 
+import torch.optim as optim
+from torchvision.models import DenseNet121_Weights
+import matplotlib.pyplot as plt
 
-path = "output/"
-
-def getDBcon():
-    try:
-        conn = mariadb.connect(
-            user=user,
-            password=password,
-            host=hostIP,
-            port=port,
-            database="homeassistant"
-        )
-    except mariadb.Error as e:
-        print(f"Error connecting to MariaDB Platform: {e}")
-        sys.exit(1)
-    return conn
-def on_connect(client, userdata, flags, code):
-    """Connect completion for Paho"""
-    _ = client
-    _ = userdata
-    _ = flags
-    global CONNECTED
-    if code == 0:
-        print("Connected to broker")
-        CONNECTED = True                #Signal connection
+lr = 0.001
+momentum = 0.9
+#weight_decay = 1.0e-4
+weight_decay = .003
+batchsize = 32
+#batchsize_valid = 64
+#start_epoch = 0
+epochs      = 50
+mean = [0.485, 0.456, 0.406]
+std = [0.229, 0.224, 0.225]
+classes = [
+        "0",
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        '7',
+        "8",
+        "9"
+        ]
+def set_device():
+    if torch.cuda.is_available():
+        print ("Using CUDA GPU")
+        dev = "cuda:0"
+    elif torch.backends.mps.is_available():
+        print ("Using Mac GPU")
+        dev = "mps"
     else:
-        print("Connection failed")
+        print ("Using CPU")
+        dev = "cpu"
+    return torch.device(dev)
+def load_model():
+    train_transforms =  transforms.Compose([transforms.Resize((224,224)),
+    transforms.ToTensor()
+    ])
+    train_dataset = torchvision.datasets.ImageFolder( root= train_dataset_path, transform=train_transforms)
+    train_loader = torch.utils.data.DataLoader (dataset= train_dataset, batch_size=batchsize, shuffle=False)
+    mean, std = get_mean_and_std(train_loader)
+    print ("Mean: " )
+    print(mean)
+    print ("Std")
+    print(std)
+#mean = [0.4868, 0.4868, 0.4868]
+#std = [0.1944, 0.1944, 0.1944]
+    train_transforms = transforms.Compose([
+    transforms.Resize((224,224)),
+    #transforms.RandomHorizontalFlip(),
+    transforms.Grayscale(num_output_channels=3),
+    #transforms.RandomRotation(10),
+    transforms.ToTensor(),
+    ThresholdTransform(thr_255=75),
+    transforms.Normalize(torch.Tensor(mean), torch.Tensor(std))
+    # transforms.Normalize((0.5), (0.5))
+    ])
+    test_transforms = transforms.Compose([
+    transforms.Resize((224,224)),
+    transforms.Grayscale(num_output_channels=3),
+    transforms.ToTensor(),
+    ThresholdTransform(thr_255=75),
+    # transforms.Normalize((0.5), (0.5))
+    transforms.Normalize(torch.Tensor(mean), torch.Tensor(std))
+    ])
+#print("Setting up model...")
+#     resnet50_model = models.resnet50(pretrained=True)
+# #resnet50_model = models.resnet50()
+# #resnet50_model = models.resnet50(weights='ResNet50_Weights.DEFAULT')
+#     num_ftrs = resnet50_model.fc.in_features
+    num_classes = 10
+#     resnet50_model.fc = nn.Linear(num_ftrs,number_of_classes)
+    weights = DenseNet121_Weights.DEFAULT  # or choose another variant if needed
+    densenet_model = models.densenet121(weights=weights)
 
-def get_average_circles(in_vals, mean=False):
-    """Compute median or mean of each column of circles in a list
-    of columns of circles"""
-    val_size = len(in_vals[0])
-    columns = []
-    for circlelist in range(val_size):
-        columns.append([])
-    for circlelist in in_vals:
-        for column in range(val_size):
-            columns[column].append(circlelist[column])
-    result = []
-    for column in columns:
-        x_coords = []
-        y_coords = []
-        radii = []
-        for circle in column:
-            x_coords.append(circle[0])
-            y_coords.append(circle[1])
-            radii.append(circle[2])
-        if mean:
-            x_average = statistics.mean(x_coords)
-            y_average = statistics.mean(y_coords)
-            radius_average = statistics.mean(radii)
-        else:
-            x_average = statistics.median(x_coords)
-            y_average = statistics.median(y_coords)
-            radius_average = statistics.median(radii)
-        result.append([x_average, y_average, radius_average])
 
-    return result
+    # Replace the classifier
+    num_ftrs = densenet_model.classifier.in_features
+    densenet_model.classifier = nn.Linear(num_ftrs, num_classes)  # Adjust 'num_classes' to your dataset
 
-def connect_mqtt():
-    """Connect to MQTT"""
-    global CONNECTED
+    # Device Configuration and Model Transfer
+    device = set_device()
+    #densenet_model.to(device)
+  
+#device = set_device()
+#resnet50_model = resnet50_model.to(device)
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(densenet_model.parameters(),lr=lr, momentum=momentum, weight_decay=weight_decay)
+# Load checkpoint if exists
+    checkpoint_path = "best_checkpoint.pth.tar"
+    checkpoint = torch.load(checkpoint_path, map_location="cpu" )
+    densenet_model.load_state_dict(checkpoint['state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer'])
+#mean = [0.4868, 0.4868, 0.4868]
+#std = [0.1944, 0.1944, 0.1944]
+    densenet_model.eval()
 
-    broker_address = "localhost"
-    port = 1883
-    timeId =  datetime.now().strftime("%H:%M:%S")
-    clientId = mqclientId + timeId
-    print(clientId)
-    client = mqttClient.Client("GasMeter", clientId)    #create new instance
-    client.on_connect = on_connect   
-    client.username_pw_set("jddayley", "java")         #attach function to callback
-    client.connect(hostIP, port=1883) #connect to broker
 
-    client.loop_start()
 
-    while not CONNECTED:
-       time.sleep(sleep_seconds)
-    return client
-
-def get_frames(num_frames):
-    """Get the number of video frames specified"""
-    frames = []
-    cap = cv2.VideoCapture(RTSP_HOST, cv2.CAP_FFMPEG)
-    print ("Connected to Wyze v3") 
-    #cap = cv2.VideoCapture(0)
-    cap.set(3, 1280)
-    cap.set(4, 1024)
-    for reading in range(num_frames):
-        _ = reading
-        ret, frame = cap.read()
-        if ret:
-            frames.append(frame)
-    cap.release()
-    return frames
-
-def get_circles(frames):
-    """Get circles from the frames"""
-    circles_list = []
-    images_list = []
-    for sample, frame in enumerate(frames):
-        try:
-            img, circles = gas_meter_reader.get_circles(frame, sample)
-            #print(circles)
-            if circles is None:
-                print ("No Circles Found")
-                continue
-            sorted_circles = sorted(circles, key=lambda circle: circle[0])
-           
-            #circles_list.append(sorted_circles)
-            #images_list.append(img)
-            if len(sorted_circles) == 4:
-                circles_list.append(sorted_circles)
-                images_list.append(img)
-        except IndexError as err:
-           # print(f"Unexpected {err=}, {type(err)=}")
-            print ("Error - Bad Index")
-            circles = None
-    if not circles_list:
-        print("Could not get any circles!")
-        circles = None
-    else:
-        circles = get_average_circles(circles_list)
-    return (circles, images_list)
-
-def main(argv):
-    """Entry point,  Allow to define an inital reading"""
-    _ = argv
-    if len(sys.argv) > 1:
-        print("reading args")
-        last_reading = float(sys.argv[1])
-    else:
-        conn = getDBcon()
-        cur = conn.cursor()
-        cur.execute(
-            "SELECT MAX(state) from homeassistant.states where metadata_id='41' AND `state` != 'unknown' AND `state` != 'unavailable'"
-            )
-        try:
-            val = float(cur.fetchone()[0])
-            print(val)
-        except TypeError:
-            #defaul value if new db.
-            val = float(8462)
-        last_reading = val 
-
-    print (argv)
-    client = connect_mqtt()
-    err_count = 0
-    #expected_range = read_rangefile(RANGEFILE)
-
+    image_transforms = transforms.Compose([
+    transforms.Grayscale(num_output_channels=3),
+    transforms.Resize((224,224)),
+    transforms.ToTensor(),
+    ThresholdTransform(thr_255=75),
+    #transforms.Normalize((0.5), (0.5))
+    transforms.Normalize(torch.Tensor(mean), torch.Tensor(std))
+])
     
-    # at 5 minute readings, that's an hour
-    circle_history = collections.deque(maxlen=12)
-    #while True:
-    now = datetime.now()
-    next_time = now + timedelta(minutes=sleep_time)
-    print("Last Reading: " + str(last_reading))
-    frames = get_frames(10)
-    print("Got %d frames" % len(frames))
+    return densenet_model,image_transforms
 
-    #gas_meter_reader.clear_debug()
-    if frames:
-        circles, images_list = get_circles(frames)
-        #if not circles:
-           # continue
-        #print("Median circles: %s" % str(circles))
-        circle_history.append(circles)
-        print(circle_history)
-        circles = get_average_circles(circle_history, mean=True)
-        print("Mean history circles: %s" % str(circles))
-        readings = []
-        for sample, image in enumerate(images_list):
-            try:
-                reading = gas_meter_reader.process(image, circles, sample)
-            except TypeError as e:
-                print("TypeError Exception:  {e!r}")
-                readings.append(1111) 
-                break
-            except AttributeError:
-                print("AttributeError Exception") 
-                readings.append(1111) 
-                break
-            except Exception as e:
-                print(f"No idea what happened: {e!r}")
-                readings.append(1111) 
-                break
-            if len(reading) == 5:
-                #print("Reading: %s" % str(reading))
-                output = gas_meter_reader.assemble_reading(reading)
-                readings.append(output)
-                
-        reading = statistics.mean(readings)
-        print("Mean reading: %s" % str(reading))
-        rounded = round(reading * 5.0) / 5.0
-        dial1 = classify(camera_path + "0-crop-0.jpg")
-        dial2 = classify(camera_path + "0-crop-1.jpg")
-        dial3 = classify(camera_path + "0-crop-2.jpg")
-        dial4 = classify(camera_path + "0-crop-3.jpg")
-        ML_predict = int(dial1 + dial2 + dial3 + dial4)
-        print ("ML Predict: " + str(ML_predict))
-        if last_reading is None:
-            print("First Time")
-            last_reading = rounded
-        elif rounded < last_reading:
-            print("*Error Check* reading is the same or too low.")
-            if (rounded > last_reading) and ((rounded -last_reading) < 10):
-                last_reading, rounded = error_check(last_reading, ML_predict)
-            else:
-                last_reading, rounded = error_check(last_reading, ML_predict)
-            #last_reading, rounded = error_check(last_reading, rounded)
-        else:
-            print("Checking: value %s rounded %s" %
-                        (str(last_reading), str(rounded)))
-            if (rounded > last_reading) and ((rounded -last_reading) < 10):
-                last_reading, rounded = error_check(last_reading, ML_predict)
-            else:
-                last_reading, rounded = error_check(last_reading, ML_predict)
-        print ("Rounded: "+  str(rounded))
-        print("Last Reading: " + str(last_reading))
-        message = {"reading": rounded,
-                    "timestamp": str(now),
-                    "raw": reading, 
-                    "ML": ML_predict
-                   }
-        if publish:
-            client.publish(mqqt_q, json.dumps(message))
-            print("Published: " + str(last_reading))
-            dt_string = now.strftime("%Y/%d/%m %H:%M:%S")
-            print("date and time =", dt_string)
-
+densenet_model, image_transforms = load_model()
+#model = densenet_model.eval()
+def set_device():
+    if torch.cuda.is_available():
+        print("Using CUDA GPU")
+        dev = "cuda:0"
+    elif torch.backends.mps.is_available():
+     #   print("Using Mac GPU")
+        dev = "mps"
     else:
-        print("Unable to read frames!")
+        print("Using CPU")
+        dev = "cpu"
+    return torch.device(dev)
+def classify(image_path):
+    image = Image.open(image_path).convert('RGB')
+    # Keep the original image for plotting
+    original_image = image.copy()
+    image = image_transforms(image).float()
+    image = image.unsqueeze(0)
+    output = densenet_model(image)
+    _, predicted = torch.max(output.data, 1)
+    # Return both the prediction and the original image
+    return classes[predicted.item()], original_image
 
-    # while datetime.now() < next_time:
-    #     time.sleep(max(timedelta(seconds=0.1),
-    #                    (next_time - datetime.now())/2).total_seconds())
 
-def error_check(last_reading, rounded):
-    
-    alter1digreading = str(last_reading)[:1] + str(rounded)[1:]
-    alter2digreading = str(last_reading)[:2] + str(rounded)[2:]
-    alter3digreading = str(last_reading)[:3] + str(rounded)[3:]
-   #print("Remove first digit and check: " + alter1digreading)
-   #print("Remove second digit and check: " + alter2digreading)
-    now = datetime.now()
-    dt_string = now.strftime("%Y/%d/%m %H:%M:%S")
-    if (int(rounded) == int(last_reading)):
-        print("Reading is the Same. %s > %s" %
-                            (str(last_reading), str(rounded)))
-        last_reading = rounded
-    else:
-        if (rounded > last_reading) and ((rounded -last_reading) < error_diff):
-            print("Good One. %s > %s" %
-                        (str(last_reading), str(rounded)))
-            last_reading = rounded
-        elif  ((float(alter1digreading) > last_reading) and (float(alter1digreading) - last_reading) < error_diff ):
-            rounded = float(alter1digreading)
-            last_reading = rounded
-            os.mkdir(camera_path + "bad/") 
-            os.rename(camera_path + "0-crop-0.jpg", camera_path + "bad/0-crop-0.jpg" + dt_string)
-            print("Recovered  - Ignore First Digit. Take the last digits %s > %s" %
-                        (str(last_reading), str(rounded)))
-        elif  ((float(alter2digreading) > last_reading) and (float(alter2digreading) - last_reading) < error_diff ):
-            rounded = float(alter2digreading)
-            last_reading = rounded
-            os.rename(camera_path + "0-crop-1.jpg", camera_path + "bad/0-crop-1.jpg"+ dt_string)
-            print("Recovered  - Ignore First and Second Digit. Take the last Two digits %s > %s" %
-                        (str(last_reading), str(rounded)))
-        elif  ((float(alter3digreading) > last_reading) and (float(alter3digreading) - last_reading) < error_diff ):
-            rounded = float(alter3digreading)
-            last_reading = rounded
-            os.rename(camera_path + "0-crop-2.jpg", camera_path + "bad/0-crop-2.jpg"+ dt_string)
-            print("Recovered  - Ignore First and Second Digit. Take the last Two digits %s > %s" %
-                        (str(last_reading), str(rounded)))
-        else:
-            print("BAD READING - no recover. Reusing last higher reading %s > %s" %
-                            (str(last_reading), str(rounded)))
-        rounded = last_reading
-
-    return last_reading,rounded
-
+def main():
+    for i in range(10):
+        path = "/Users/ddayley/Desktop/gas/data/images_copy/" + str(i)
+        files = os.listdir(path)
+        for file in files:
+            if file != ".DS_Store":
+                guess, original_image = classify(os.path.join(path, file))  # Now returns original image as well
+                if str(i) != guess:
+                    print(str(i) + " " + file + " Debug-Predict: " + guess)
+                    # Display the original image
+                    plt.imshow(original_image)
+                    plt.title("Failed Image: Expected {}, Predicted {}".format(i, guess))
+                    plt.show()
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    main()
